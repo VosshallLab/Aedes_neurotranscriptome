8622ca414cd7f0d0a9834891e296903a62fcdb6a
ben matthews <bmatthews@rockefeller.edu>
2015-07-23 11:06:55 -0400

    tidying up directory structure

--- DESeq_initialize.R
+++ DESeq_initialize.R
@@ -5,21 +5,21 @@ library(gplots)
 
 source('ntx_deseq_functions.R')
 
-setwd('~/Dropbox/ntx_deseq/')
+setwd('~/bioinfo/github/ntx_deseq/')
 
-PE <- read.csv('PE_featurecounts.txt',sep = "\t",comment.char = "#")
-SE <- read.csv('SE_featurecounts.txt',sep = "\t",comment.char = "#")
+PE <- read.csv('raw_gene_counts/PE_featurecounts.txt',sep = "\t",comment.char = "#")
+SE <- read.csv('raw_gene_counts/SE_featurecounts.txt',sep = "\t",comment.char = "#")
 
 merge(PE,SE,by=c("Geneid","Chr","Start","End","Strand","Length")) -> test
 
-columns <- read.csv('test.csv')
+columns <- read.csv('annotations/library_key.csv')
 colnames(test) <- columns$y
 row.names(test) <- test$gene
 raw_FC <- test[,7:140]
 
 ##### read in library information from CSV file
 
-libprop <- read.csv('libraries.csv')
+libprop <- read.csv('annotations/library_info.csv')
 row.names(libprop) <- libprop$library
 raw_FC_reordered <- raw_FC[row.names(libprop)]
 
