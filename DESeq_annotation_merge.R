a34bc970f5f45b189e10ec9abceb336983884a80
ben matthews <bmatthews@rockefeller.edu>
2015-07-24 13:52:36 -0400

    added gene families to annotations for downstream analyses

--- DESeq_annotation_merge.R
+++ DESeq_annotation_merge.R
@@ -4,11 +4,12 @@ PE <- read.csv('raw_gene_counts/PE_featurecounts.txt',sep = "\t",comment.char = 
 SE <- read.csv('raw_gene_counts/SE_featurecounts.txt',sep = "\t",comment.char = "#")
 
 gene_annotations <- merge(PE,SE,by=c("Geneid","Chr","Start","End","Strand","Length"))
+row.names(gene_annotations) <- gene_annotations$Geneid
 gene_annotations <- subset(gene_annotations, select=c("Geneid","Chr","Start","End","Strand","Length"))
 
 manual_annotations <- read.csv('~/Dropbox/writing/Aedes neurotranscriptome paper/CURRENT DRAFT/Supplemental data/Additional File 4 - AaegL2.1RU gene description DONE/Additional file 4 - AaegL2.1RU.csv')
 row.names(manual_annotations) <- manual_annotations$internal.gene_id
 
-gene_annotations <- merge(gene_annotations, manual_annotations)
+gene_annotations <- merge(gene_annotations, manual_annotations,by="row.names")
 
 write.csv(gene_annotations,'annotations/AaegL2.1RUv2_annotations.csv')
