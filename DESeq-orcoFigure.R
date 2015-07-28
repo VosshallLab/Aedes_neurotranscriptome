e4de3317d16b53fbdcaabe1e7216273a216c1dfa
ben matthews <bmatthews@rockefeller.edu>
2015-07-28 19:59:26 -0400

    done for night

--- DESeq-orcoFigure.R
+++ DESeq-orcoFigure.R
@@ -7,4 +7,4 @@ grab_genefamily <- function(genefam,tpmMat) {
 OR <- grab_genefamily("OR",compiled_means)
 
 OR_plot_An <- ggplot(data=OR,aes(x=log10(Fe_An_SF+1),y=log10(Ma_An+1))) + geom_point() + theme_bw()
-  OR_plot_Rs <- ggplot(data=OR,aes(x=log10(Fe_Rs_SF+1),y=log10(Ma_Rs+1))) + geom_point() + theme_bw()
+OR_plot_Rs <- ggplot(data=OR,aes(x=log10(Fe_Rs_SF+1),y=log10(Ma_Rs+1))) + geom_point() + theme_bw()
