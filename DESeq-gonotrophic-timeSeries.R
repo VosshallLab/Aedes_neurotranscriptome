dca980476503da10ebc11d8bb9e5833bdba1b006
ben matthews <bmatthews@rockefeller.edu>
2015-07-29 15:39:44 -0400

    added z-score maps for gonotrophic signalling

--- DESeq-gonotrophic-timeSeries.R
+++ DESeq-gonotrophic-timeSeries.R
@@ -5,14 +5,85 @@
 # dds_brain_gono
 # dds_antenna_gono
 
-
+####################
 # brain as example
-resBrain_sfbf <- results(dds_brain_gono,contrast=c("condition","SF","BF"))
-resBrain_sfo <- results(dds_brain_gono,contrast=c("condition","SF","O"))
+resbrain_sfbf <- results(dds_brain_gono,contrast=c("condition","SF","BF"))
+resbrain_sfo <- results(dds_brain_gono,contrast=c("condition","SF","O"))
 
-brain_modulated <- vstMat_brain[resBrain_sfbf$padj < 0.01 | resBrain_sfo$padj < 0.01,]
-brain_modulated <- brain_modulated[,c("Fe_Br_SF_5","Fe_Br_SF_6","Fe_Br_SF_7","Fe_Br_SF_8","Fe_Br_BF_1","Fe_Br_BF_2","Fe_Br_BF_3","Fe_Br_BF_4","Fe_Br_O_1","Fe_Br_O_2","Fe_Br_O_3","Fe_Br_O_4","Fe_Br_O_5")]
+brain_modulated <- vstMat_brain[resbrain_sfbf$padj < 0.01 | resbrain_sfo$padj < 0.01,]
+row.names(brain_modulated[!is.na(row.names(brain_modulated)),]) -> modGenesbrain
 
-row.names(brain_modulated[!is.na(row.names(brain_modulated)),]) -> modGenesBrain
+brain_FCs <- data.frame("row.names" = row.names(resbrain_sfbf), "sfbf" = resbrain_sfbf$log2FoldChange, "sfo" = resbrain_sfo$log2FoldChange)
 
-heatmap.2(log10(1+vstMat_brain[row.names(vstMat_brain) %in% modGenesBrain,]))
+#heatmap.2(as.matrix(brain_FCs[row.names(brain_FCs) %in% modGenesbrain,]),trace="none",col=colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255))
+
+#################
+## antenna
+resantenna_sfbf <- results(dds_antenna_gono,contrast=c("condition","SF","BF"))
+resantenna_sfo <- results(dds_antenna_gono,contrast=c("condition","SF","O"))
+
+
+antenna_modulated <- vstMat_antenna[resantenna_sfbf$padj < 0.01 | resantenna_sfo$padj < 0.01,]
+row.names(antenna_modulated[!is.na(row.names(antenna_modulated)),]) -> modGenesantenna
+
+antenna_FCs <- data.frame("row.names" = row.names(resantenna_sfbf), "sfbf" = resantenna_sfbf$log2FoldChange, "sfo" = resantenna_sfo$log2FoldChange)
+
+#heatmap.2(as.matrix(antenna_FCs[row.names(antenna_FCs) %in% modGenesantenna,]),trace="none",breaks=seq(-2,2,length.out=256),col=colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255))
+
+#################
+## hindlegs
+reshindlegs_sfbf <- results(dds_hindlegs_gono,contrast=c("condition","SF","BF"))
+reshindlegs_sfo <- results(dds_hindlegs_gono,contrast=c("condition","SF","O"))
+
+
+hindlegs_modulated <- vstMat_hindlegs[reshindlegs_sfbf$padj < 0.01 | reshindlegs_sfo$padj < 0.01,]
+row.names(hindlegs_modulated[!is.na(row.names(hindlegs_modulated)),]) -> modGeneshindlegs
+
+hindlegs_FCs <- data.frame("row.names" = row.names(reshindlegs_sfbf), "sfbf" = reshindlegs_sfbf$log2FoldChange, "sfo" = reshindlegs_sfo$log2FoldChange)
+
+#heatmap.2(as.matrix(hindlegs_FCs[row.names(hindlegs_FCs) %in% modGeneshindlegs,]),trace="none",breaks=seq(-2,2,length.out=256),col=colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255))
+
+#################################
+# z-score clustering and heatmaps
+########## brain
+
+expMat <- data.frame("sf" = resbrain_sfbf$baseMean,"bf"=resbrain_sfbf$baseMean * (2^resbrain_sfbf$log2FoldChange),"o"=resbrain_sfo$baseMean * (2^resbrain_sfo$log2FoldChange))
+row.names(expMat) = row.names(reshindlegs_sfbf)
+
+## z-score heatmap
+z.expMat <- as.data.frame(t(apply((expMat[row.names(expMat) %in% modGenesbrain,]), MARGIN = 1, FUN = scale )))
+
+heatmap.2(as.matrix(z.expMat),
+          trace="none",symm=TRUE,Colv="none",
+          col=colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),dendrogram="none",
+          labCol = c("non-blood-fed","blood-fed","gravid"),main="brain",trace="none")
+
+#################################
+# z-score clustering and heatmaps
+########## antenna
+
+expMat <- data.frame("sf" = resantenna_sfbf$baseMean,"bf"=resantenna_sfbf$baseMean * (2^resantenna_sfbf$log2FoldChange),"o"=resantenna_sfo$baseMean * (2^resantenna_sfo$log2FoldChange))
+row.names(expMat) = row.names(reshindlegs_sfbf)
+
+## z-score heatmap
+z.expMat <- as.data.frame(t(apply((expMat[row.names(expMat) %in% modGenesantenna,]), MARGIN = 1, FUN = scale )))
+
+heatmap.2(as.matrix(z.expMat),
+          trace="none",symm=TRUE,Colv="none",
+          col=colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),dendrogram="none",
+          labCol = c("non-blood-fed","blood-fed","gravid"),main="antenna",trace="none")
+
+#################################
+# z-score clustering and heatmaps
+########## hindlegs
+
+expMat <- data.frame("sf" = reshindlegs_sfbf$baseMean,"bf"=reshindlegs_sfbf$baseMean * (2^reshindlegs_sfbf$log2FoldChange),"o"=reshindlegs_sfo$baseMean * (2^reshindlegs_sfo$log2FoldChange))
+row.names(expMat) = row.names(reshindlegs_sfbf)
+
+## z-score heatmap
+z.expMat <- as.data.frame(t(apply((expMat[row.names(expMat) %in% modGeneshindlegs,]), MARGIN = 1, FUN = scale )))
+
+heatmap.2(as.matrix(z.expMat),
+          trace="none",symm=TRUE,Colv="none",
+          col=colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),dendrogram="none",
+          labCol = c("non-blood-fed","blood-fed","gravid"),main="hindlegs",trace="none")
--- ntx_figure_flowchart.R
+++ ntx_figure_flowchart.R
@@ -12,4 +12,8 @@ source('~/bioinfo/github/ntx_deseq/TPM_plots.R')
 
 source('~/bioinfo/github/ntx_deseq/DESeq_gonotrophic.R')
 
+source('~/bioinfo/github/ntx_deseq/DESeq_gonotrophic_contrasts.R')
+
 source('~/bioinfo/github/ntx_deseq/DESeq-orcoFigure.R')
+
+source('~/bioinfo/github/ntx_deseq/DESeq-gonotrophic-timeSeries.R')
